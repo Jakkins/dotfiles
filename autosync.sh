01889cc74b41efbe5464bce26806f1128b98d8fd
jak <none>
2024-07-04 12:26:40 +0000

    update autosync is now generalized for config inside ~/.config dir, add save terminator conf

--- autosync.sh
+++ autosync.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
 
-BASE_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-
 # save ~/.config/hypr/hyprland.conf
-mkdir -p .config/hypr
-rm -f $BASE_DIR/.config/hypr/hyprland.conf
-cp ~/.config/hypr/hyprland.conf ./.config/hypr/hyprland.conf
-
 # save ~/.config/waybar
-rm -rf $BASE_DIR/.config/waybar
-mkdir -p .config/waybar
-cp ~/.config/waybar/* .config/waybar/
+# save ~/.config/terminator
+declare -a configs=("hypr" "waybar" "terminator")
 
-
+for config in "${configs[@]}"; do
+  local_config_path=".config/$config"
+  
+  rm -rf $local_config_path
+  mkdir -p $local_config_path
+  cp ~/.config/$config/* $local_config_path/
+done
